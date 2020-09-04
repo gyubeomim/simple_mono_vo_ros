70f4642bde8fbbbc7a6a049f2b376b0fffdd255b
edward <gyurse@gmail.com>
2020-09-04 18:19:27 +0900

    add comment

--- src/mono_vo_node.cpp
+++ src/mono_vo_node.cpp
@@ -13,15 +13,16 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "mono_vo_node");
   ros::NodeHandle nh, priv_nh("~");
 
+  // Set publishers.
   ros::Publisher pub_odom = nh.advertise<nav_msgs::Odometry>("odom", 1);
   ros::Publisher pub_image = nh.advertise<sensor_msgs::Image>("image", 1);
 
-  int max_frame;
-  int min_num_feat;
-  std::string fn_kitti;
+  int max_frame;          // maximum frame to play in KITTI sequence.
+  int min_num_pts;       // minimum number of feature points.
+  std::string fn_kitti;   // path to KITTI dataset.
 
-  priv_nh.getParam("max_frame", max_frame); // 1000
-  priv_nh.getParam("min_num_feat", min_num_feat); // 2000
+  priv_nh.getParam("max_frame", max_frame);
+  priv_nh.getParam("min_num_pts", min_num_pts);
   priv_nh.getParam("fn_kitti", fn_kitti);
 
   tf::TransformBroadcaster tf_broadcaster;
@@ -29,16 +30,20 @@ int main(int argc, char **argv) {
   transform.frame_id_ = "/world";
   transform.child_frame_id_ = "/camera";
 
-  MonoVO* vo = new MonoVO(max_frame, min_num_feat, fn_kitti);
+  // Create MonoVO instance.
+  MonoVO* vo = new MonoVO(max_frame, min_num_pts, fn_kitti);
 
-  int nframe=2;
+  // Starts from the third image (first and second images are used for the initialization).
+  int nframe = 2;
   while(ros::ok()) {
     if(nframe >= max_frame) {
       break;
     }
 
+    // Perform pose tracking for the n-th frame.
     vo->PoseTracking(nframe);
 
+    // Get rotation and translation from vo.
     cv::Mat R = vo->GetRotation();
     cv::Mat t_wc = vo->GetTranslation();
     cv::Vec3f euler = util::RotMatToEuler(R);
@@ -46,8 +51,9 @@ int main(int argc, char **argv) {
                      R.at<double>(1,0), R.at<double>(1,1), R.at<double>(1,2),
                      R.at<double>(2,0), R.at<double>(2,1), R.at<double>(2,2));
     tf::Quaternion quat;
-    _R.getRotation(quat);
+    _R.getRotation(quat); // convert rotation matrix to quaternion.
 
+    // Set the digit precision.
     cv::Ptr<cv::Formatter> round = cv::Formatter::get(cv::Formatter::FMT_DEFAULT);
     round->set64fPrecision(3);
     round->set32fPrecision(3);
@@ -59,6 +65,8 @@ int main(int argc, char **argv) {
     transform.stamp_ = ros::Time::now();
     transform.setRotation(tf::Quaternion(quat[0], quat[1], quat[2], quat[3]));
     transform.setOrigin(tf::Vector3(t_wc.at<double>(0), t_wc.at<double>(1), t_wc.at<double>(2)));
+
+    // Braodcast the transform between /world and /camera.
     tf_broadcaster.sendTransform(transform);
 
     nav_msgs::Odometry odom;
@@ -72,7 +80,7 @@ int main(int argc, char **argv) {
     odom_quat.z = quat[2];
     odom_quat.w = quat[3];
 
-    //set the position
+    // Set the position and rotation.
     odom.pose.pose.position.x = t_wc.at<double>(0);
     odom.pose.pose.position.y = t_wc.at<double>(1);
     odom.pose.pose.position.z = t_wc.at<double>(2);
@@ -81,10 +89,9 @@ int main(int argc, char **argv) {
     // publish to /odom.
     pub_odom.publish(odom);
 
+    // Get the result image from vo.
     cv::Mat dst = vo->GetResultImage();
-
     cv_bridge::CvImagePtr cv_ptr(new cv_bridge::CvImage);
-
     cv_ptr->encoding = "bgr8";
     cv_ptr->header.stamp = ros::Time::now();
     cv_ptr->header.frame_id = "/world";
@@ -93,6 +100,7 @@ int main(int argc, char **argv) {
     // publish to /image.
     pub_image.publish(cv_ptr->toImageMsg());
 
+    // Increase the number of frame.
     nframe++;
   }
 
