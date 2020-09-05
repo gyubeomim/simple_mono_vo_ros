da0d214a496c3a0f263d341d5f5f7d06d94d4df3
edward <gyurse@gmail.com>
2020-09-05 19:22:32 +0900

    add comment

--- src/util.h
+++ src/util.h
@@ -14,10 +14,20 @@
 
 namespace util {
 
+/// \brief Add zero padding in the file name. It's used in the case that you should type '000001.png'.
+/// \param[in] value The number of file name.
+/// \param[in] precision Zero padding precision.
+/// \return Zero padded string.
 std::string AddZeroPadding(const int value, const unsigned precision);
 
+/// \brief This method checks whether the rotations matrix is valid or not.
+/// \param[in] R Rotation matrix.
+/// \return Boolean of the result. If R is the rotation matrix, return true.
 bool IsRotMat(cv::Mat &R);
 
+/// \brief Convert rotation matrix to euler angle.
+/// \param[in] R Rotation matrix.
+/// \return Euler angle.
 cv::Vec3f RotMatToEuler(cv::Mat &R);
 
 }// namespace util
